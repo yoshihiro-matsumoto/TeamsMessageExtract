a60fdd7ed024baa97b0f9af9cd50aa834d137a64
Yoshihiro Matsumoto <yomatsum@microsoft.com>
2019-07-02 11:46:42 +0900

    add user export.

--- Extract.ps1
+++ Extract.ps1
@@ -1,13 +1,14 @@
 function Export-TeamsMessagesAndReplies {  
     param (
-        $ExportMessagePath,
-        $ExportRepliesPath,
+        $ExportPath,
         $TeamId,
         $ChannelId,
-        $AccessToken
+        $AccessToken,
+        $QueryFilter
     )  
     process {
         $messageList = Invoke-RestMethod -Headers @{Authorization = "Bearer $AccessToken" } -Uri https://graph.microsoft.com/beta/teams/$TeamId/channels/$ChannelId/messages -Method Get
+        $userList = Invoke-RestMethod -Headers @{Authorization = "Bearer $AccessToken"} -Uri https://graph.microsoft.com/v1.0/users?$QueryFilter -Method Get
         $repliesList = @()
 
         foreach ($message in $messageList.value) {
@@ -18,12 +19,15 @@ function Export-TeamsMessagesAndReplies {
             Catch {
 
             }
-            $repilesList += $replies.value
+            $repliesList += $replies.value
         }
 
-        $messageList.value | ConvertTo-Json | Out-File $ExportMessagePath
-        $repilesList | ConvertTo-Json | Out-File $ExportRepliesPath
+        $userList.value | ConvertTo-Json -Depth 10 | Out-File $ExportPath\\User.json
+        $messageList.value | ConvertTo-Json -Depth 10 | Out-File $ExportPath\\Messages.json
+        $repliesList | ConvertTo-Json -Depth 10 | Out-File $ExportPath\\Replies.json
     }
 }
 
-Export-TeamsMessagesAndReplies -ExportMessagePath ".\Messages.json" -ExportRepliesPath ".\Replies.json" -TeamId "<your team guid>" -ChannelId "<your channel id>" -AccessToken "<your access token>"
+# Export-TeamsMessagesAndReplies -ExportPath "." -TeamId "<your teams id>" -ChannelId "<your channel id>" `
+#                                -QueryFilter "`$filter=Department eq '<your department>'" `
+#                                -AccessToken "<your access token for graph api>"
